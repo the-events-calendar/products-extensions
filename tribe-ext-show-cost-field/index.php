ba63ffebbede3db80b44f96ad9bc4a24c720c75b
Brook <brook@eli.mn>
2016-12-07 03:00:42 -0700

    Committed the right file this time.

--- tribe-ext-show-cost-field/index.php
+++ tribe-ext-show-cost-field/index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name:     Event Tickets Extension: Additional Settings
- * Description:     Adds a few extra settings to the general settings tab that are missing when The Events Calendar is not active.
+ * Plugin Name:     Event Tickets Plus Extension: Show Cost Fields
+ * Description:     Event Tickets Plus hides the basic cost fields when activated, and automatically overrides the info with the costs of your tickets. When this extension is activated alongside Event Tickets Plus it will restore the hidden fields, allowing you to manually set the data.
  * Version:         1.0.0
- * Extension Class: Tribe__Extension__Ticket_Currency_Settings
+ * Extension Class: Tribe__Extension__Show_Cost_Field
  * Author:          Modern Tribe, Inc.
  * Author URI:      http://m.tri.be/1971
  * License:         GPLv2 or later
@@ -11,14 +11,18 @@
  */
 
 // Do not load directly.
-if ( ! defined( 'ABSPATH' ) ) { die( '-1' ); }
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
 // Do not load unless Tribe Common is fully loaded.
-if ( ! class_exists( 'Tribe__Extension' ) ) { return; }
+if ( ! class_exists( 'Tribe__Extension' ) ) {
+	return;
+}
 
 /**
  * Extension main class, class begins loading on init() function.
  */
-class Tribe__Extension__Ticket_Currency_Settings extends Tribe__Extension {
+class Tribe__Extension__Show_Cost_Field extends Tribe__Extension {
 
 	/**
 	 * Setup the Extension's properties.
@@ -27,44 +31,14 @@ class Tribe__Extension__Ticket_Currency_Settings extends Tribe__Extension {
 	 */
 	public function construct() {
 		$this->add_required_plugin( 'Tribe__Tickets__Main' );
+		$this->add_required_plugin( 'Tribe__Tickets_Plus__Main' );
+		$this->set_url( 'https://theeventscalendar.com/extensions/show-cost-fields/' );
 	}
 
 	/**
 	 * Extension initialization and hooks.
 	 */
 	public function init() {
-		// Settings area.
-		require_once dirname( __FILE__ ) . '/src/Tribe/Settings_Helper.php';
-		add_action( 'admin_init', array( $this, 'add_settings' ) );
-	}
-
-	/**
-	 * Adds settings options
-	 */
-	public function add_settings() {
-		$setting_helper = new Tribe__Settings_Helper();
-
-		$fields = array(
-			'defaultCurrencySymbol'         => array(
-				'type'            => 'text',
-				'label'           => esc_html__( 'Default currency symbol', 'the-events-calendar' ),
-				'tooltip'         => esc_html__( 'Set the default currency symbol for event costs. Note that this only impacts future events, and changes made will not apply retroactively.', 'the-events-calendar' ),
-				'validation_type' => 'textarea',
-				'size'            => 'small',
-				'default'         => '$',
-			),
-			'reverseCurrencyPosition'       => array(
-				'type'            => 'checkbox_bool',
-				'label'           => esc_html__( 'Currency symbol follows value', 'the-events-calendar' ),
-				'tooltip'         => esc_html__( 'The currency symbol normally precedes the value. Enabling this option positions the symbol after the value.', 'the-events-calendar' ),
-				'default'         => false,
-				'validation_type' => 'boolean',
-			),
-		);
-
-		$setting_helper->add_fields(
-			$fields,
-			'general'
-		);
+		add_filter( 'tribe_events_admin_show_cost_field', '__return_true', 100 );
 	}
 }
