0b034cc5221d45e5850fc9cfe091cb40512194dd
George Gecewicz <gecewicz.george@gmail.com>
2016-12-07 21:17:50 -0500

    implementing gustavo&brook's changes

--- tribe-ext-link-events-to-website-url/index.php
+++ tribe-ext-link-events-to-website-url/index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name:     The Events Calendar: Link Event Titles to Event Website URLs
+ * Plugin Name:     The Events Calendar Extension: Link Events to Website URLs
  * Description:     An extension that makes event titles link to the events' Website URLs when present.
  * Version:         1.0.0
- * Extension Class: Tribe__Extension__Link_Event_Titles_to_Event_Website_URLs
+ * Extension Class: Tribe__Extension__Link_Events_to_Website_URLs
  * Author:          Modern Tribe, Inc.
  * Author URI:      http://m.tri.be/1971
  * License:         GPLv2 or later
@@ -20,23 +20,23 @@ if ( ! class_exists( 'Tribe__Extension' ) ) {
 	return;
 }
 
-class Tribe__Extension__Link_Event_Titles_to_Event_Website_URLs extends Tribe__Extension {
+class Tribe__Extension__Link_Events_to_Website_URLs extends Tribe__Extension {
 
-    /**
-     * Setup the Extension's properties.
-     *
-     */
-    public function construct() {
-        $this->add_required_plugin( 'Tribe__Events__Main', '4.3.3' );
-        $this->set_url( 'https://theeventscalendar.com/extensions/make-event-titles-link-to-the-event-website-url/' );
-    }
+	/**
+	 * Setup the Extension's properties.
+	 *
+	 */
+	public function construct() {
+		$this->add_required_plugin( 'Tribe__Events__Main', '4.3.3' );
+		$this->set_url( 'https://theeventscalendar.com/extensions/make-event-titles-link-to-the-event-website-url/' );
+	}
 
-    /**
-     * Extension initialization and hooks.
-     */
-    public function init() {
-    	add_filter( 'tribe_get_event_link', array( $this, 'make_event_titles_link_to_event_website' ), 100, 2 );
-    }
+	/**
+	 * Extension initialization and hooks.
+	 */
+	public function init() {
+		add_filter( 'tribe_get_event_link', array( $this, 'tribe_get_event_link' ), 100, 2 );
+	}
 
 	/**
 	 * Make event titles link to URLs from their respective "Event Website" fields.
@@ -45,14 +45,14 @@ class Tribe__Extension__Link_Event_Titles_to_Event_Website_URLs extends Tribe__E
 	 * @param int $post_id
 	 * @return string
 	 */
-	public function make_event_titles_link_to_event_website( $link, $post_id ) {
-	    
-	    $website_url = tribe_get_event_website_url( $post_id );
-	
-	    if ( ! empty( $website_url ) ) {
-	        $link = $website_url;
-	    }
-	
-	    return $link;
+	public function tribe_get_event_link( $link, $post_id ) {
+
+		$website_url = tribe_get_event_website_url( $post_id );
+		
+		if ( ! empty( $website_url ) ) {
+			$link = $website_url;
+		}
+		
+		return $link;
 	}
-}
+	
