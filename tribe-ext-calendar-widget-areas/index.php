99d6cb9fb145a6002d542cae6c6767afedeab4b6
Clifford Paulick <tko@tourkick.com>
2017-06-15 03:16:58 -0500

    Refactored to enable hook priorities, new hooks
    - Previously used hook as option array key but that didn't allow using same_hook_name with a different priority... so Refactor!
    - Reformatted code to match standards
    - Added new widget areas for Single Events page

--- tribe-ext-calendar-widget-areas/index.php
+++ tribe-ext-calendar-widget-areas/index.php
@@ -1,17 +1,14 @@
 <?php
 /**
-* Plugin Name: The Events Calendar Extension: Calendar Widget Areas
-* Description: Adds widget areas (a.k.a. sidebars) that only display on The Events Calendar pages/views. Areas may be enabled or disabled at wp-admin > Events > Settings > Display tab > Advanced Template Settings section. Note that the WP Customizer only allows you to manage widget areas that apply to the page you're currently previewing; therefore, you will need to navigate to your Events page, for example, to edit the content of those widget areas via the Customizer's live preview.
-* Version: 1.0
-* Extension Class: Tribe__Extension__Calendar_Widget_Areas
-* Author: Modern Tribe, Inc.
-* Author URI: http://m.tri.be/1971
-* License: GPLv2 or later
-* License URI: https://www.gnu.org/licenses/gpl-2.0.html
-*/
-
-// Created 2015-10-05 for https://theeventscalendar.com/support/forums/topic/control-visibility-of-sidebar-widgets/#dl_post-1011743
-// Turned into an Extension by Cliff on 2017-01-11
+ * Plugin Name: The Events Calendar Extension: Calendar Widget Areas
+ * Description: Adds widget areas (a.k.a. sidebars) that only display on The Events Calendar pages/views. Areas may be enabled or disabled at wp-admin > Events > Settings > Display tab > Advanced Template Settings section. Note that the WP Customizer only allows you to manage widget areas that apply to the page you're currently previewing; therefore, you will need to navigate to your Events page, for example, to edit the content of those widget areas via the Customizer's live preview.
+ * Version: 1.0.0
+ * Extension Class: Tribe__Extension__Calendar_Widget_Areas
+ * Author: Modern Tribe, Inc.
+ * Author URI: http://m.tri.be/1971
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
 // Do not load directly.
 if ( ! defined( 'ABSPATH' ) ) {
@@ -24,8 +21,8 @@ if ( ! class_exists( 'Tribe__Extension' ) ) {
 }
 
 /**
-* Extension main class, class begins loading on init() function.
-*/
+ * Extension main class, class begins loading on init() function.
+ */
 class Tribe__Extension__Calendar_Widget_Areas extends Tribe__Extension {
 	/**
 	 * Option key for which widget areas are enabled
@@ -44,118 +41,181 @@ class Tribe__Extension__Calendar_Widget_Areas extends Tribe__Extension {
 	protected function construct() {
 		// Each plugin required by this extension
 		$this->add_required_plugin( 'Tribe__Events__Main' );
-				
+
 		// Set the extension's TEC URL
 		$this->set_url( 'https://theeventscalendar.com/extensions/calendar-widget-areas/' );
 	}
-	
+
 	/**
 	 * All available widget areas this extension supports and details about each in a multidimensional associative array.
 	 *
 	 * Follow this format to enable a new widget area for each action or filter hook
-	 * you want to use. You will also need to add an additional method toward the 
+	 * you want to use. You will also need to add an additional method toward the
 	 * end of this file for each area added so we know what code to execute.
-	 * 
+	 *
 	 * @return array {
 	 *     Every widget area supported by this extension
-	 * 
-	 *     @param array $hook {
-	 *         The details for each widget area
-	 * 
-	 *         @param string $method	The method of this class that outputs the widget. This value may prefix with context, such as "single_", and the actual function named accordingly and having the necessary logic in place. The actual function names are "tec_ext_widget_areas__" + the 'method' name from here.
-	 *         @param string $name		Name of the widget in the admin screens
-	 *         @param string $desc		Widget description
-	 *         @param string $filter	If $hook is a 'filter' hook instead of an action hook, set this to (bool) true.
-	 *     }
-	 * }
+	 *
+	 *                         {
+	 *                         The details for each widget area
+	 *
+	 * @param array  $hook     The action or filter hook to be used.
+	 * @param string $method   The method of this class that outputs the widget. This value may prefix with context, such as "single_", and the actual function named accordingly and having the necessary logic in place. The actual function names are "tec_ext_widget_areas__" + the 'method' name from here.
+	 * @param string $name     Name of the widget in the admin screens
+	 * @param string $desc     Widget description
+	 * @param bool   $filter   Optional: If $hook is a 'filter' hook instead of an action hook, set this to (bool) true.
+	 * @param int    $priority Optional: Set this if desired. Will default to 10 if not set.
+	 *                         }
+	 *                         }
 	 */
-	public function get_all_areas() {
+	protected function get_all_areas() {
 		$areas = array(
-			'tribe_events_before_template'	=>	array(
-				'method'  	=> 'before_template',
-				'name'		=> __( 'TEC Above Calendar', 'tribe-extension' ),
-				'desc'		=> __( 'Widgets in this area will be shown ABOVE The Events Calendar.', 'tribe-extension' ),
-			),				
-			'tribe_events_after_template'	=>	array(
-				'method'  	=> 'after_template',
-				'name'		=> __( 'TEC Below Calendar', 'tribe-extension' ),
-				'desc'		=> __( 'Widgets in this area will be shown BELOW The Events Calendar.', 'tribe-extension' ),
-			),				
-			'tribe_events_before_view'	=>	array(
-				'method'  	=> 'single_before_view',
-				'name'		=> __( 'TEC Above Single Events', 'tribe-extension' ),
-				'desc'		=> __( 'Widgets in this area will be shown ABOVE Single Events.', 'tribe-extension' ),
-			),				
-			'tribe_events_after_view'	=>	array(
-				'method'  	=> 'single_after_view',
-				'name'		=> __( 'TEC Below Single Events', 'tribe-extension' ),
-				'desc'		=> __( 'Widgets in this area will be shown BELOW Single Events.', 'tribe-extension' ),
+			// template
+			array(
+				'hook'   => 'tribe_events_before_template',
+				'method' => 'before_template',
+				'name'   => __( 'TEC Above Calendar', 'tribe-extension' ),
+				'desc'   => __( 'Widgets in this area will be shown ABOVE The Events Calendar.', 'tribe-extension' ),
+			),
+			array(
+				'hook'   => 'tribe_events_after_template',
+				'method' => 'after_template',
+				'name'   => __( 'TEC Below Calendar', 'tribe-extension' ),
+				'desc'   => __( 'Widgets in this area will be shown BELOW The Events Calendar.', 'tribe-extension' ),
+			),
+			// single
+			// template
+			array(
+				'hook'   => 'tribe_events_before_view',
+				'method' => 'single_before_view',
+				'name'   => __( 'TEC Single: Top', 'tribe-extension' ),
+				'desc'   => __( 'Widgets in this area will be shown ABOVE Single Events.', 'tribe-extension' ),
+			),
+			// description
+			array(
+				'hook'   => 'tribe_events_single_event_before_the_content',
+				'method' => 'single_event_before_the_content',
+				'name'   => __( 'TEC Single: Above Description', 'tribe-extension' ),
+				'desc'   => __( 'Widgets in this area will be shown ABOVE the Single Event Description.', 'tribe-extension' ),
+			),
+			// description
+			array(
+				'hook'   => 'tribe_events_single_event_after_the_content',
+				'method' => 'single_event_after_the_content',
+				'name'   => __( 'TEC Single: Below Description', 'tribe-extension' ),
+				'desc'   => __( 'Widgets in this area will be shown BELOW the Single Event Description.', 'tribe-extension' ),
+			),
+			// details
+			array(
+				'hook'   => 'tribe_events_single_event_before_the_meta',
+				'method' => 'single_event_before_the_meta',
+				'name'   => __( 'TEC Single: Above Details', 'tribe-extension' ),
+				'desc'   => __( 'Widgets in this area will be shown ABOVE the Single Event Details.', 'tribe-extension' ),
+			),
+			// details
+			array(
+				'hook'     => 'tribe_events_single_event_after_the_meta',
+				'method'   => 'single_event_after_the_meta_early',
+				'name'     => __( 'TEC Single: Below Details (Earlier)', 'tribe-extension' ),
+				'desc'     => __( 'Widgets in this area will be shown DIRECTLY BELOW the Single Event Details (above Related Events and Tickets, if displayed).', 'tribe-extension' ),
+				'priority' => 1,
+			),
+			// details
+			array(
+				'hook'     => 'tribe_events_single_event_after_the_meta',
+				'method'   => 'single_event_after_the_meta_late',
+				'name'     => __( 'TEC Single: Below Details (Later)', 'tribe-extension' ),
+				'desc'     => __( 'Widgets in this area will be shown BELOW the Single Event Details (below Related Events and Tickets, if displayed).', 'tribe-extension' ),
+				'priority' => 100,
+			),
+			// template
+			array(
+				'hook'   => 'tribe_events_after_view',
+				'method' => 'single_after_view',
+				'name'   => __( 'TEC Single: Bottom', 'tribe-extension' ),
+				'desc'   => __( 'Widgets in this area will be shown BELOW Single Events.', 'tribe-extension' ),
 			),
 		);
-		
+
 		return apply_filters( 'tribe_ext_calendar_widget_areas', $areas );
 	}
-	
+
+	/**
+	 * Get all method names
+	 *
+	 * @return array
+	 */
+	protected function get_all_areas_simple() {
+		return wp_list_pluck( $this->get_all_areas(), 'method' );
+	}
+
+	/**
+	 * Convert All Areas array from indexed array to associative array, using 'method' as the key.
+	 *
+	 * @return array
+	 */
+	protected function get_all_areas_assoc() {
+		$all_available_assoc = array();
+
+		foreach ( $this->get_all_areas() as $value ) {
+			$method_name                       = $value['method'];
+			$all_available_assoc[$method_name] = $value;
+		}
+
+		return $all_available_assoc;
+	}
+
+
 	/**
 	 * Build options to present to user
-	 * 
+	 *
 	 * @return array
 	 */
-	public function get_available_area_options() {
+	protected function get_available_area_options() {
 		$options = array();
-		
-		foreach ( $this->get_all_areas() as $key => $value ) {
-			$options[$key] = $value['name'];
+
+		foreach ( $this->get_all_areas() as $value ) {
+			$method_name           = $value['method'];
+			$options[$method_name] = $value['name'];
 		}
-		
+
 		return $options;
 	}
-	
+
 	/**
-	 * The chosen widget areas to activate/run. Just the array keys from get_all_areas()
-	 * 
+	 * The chosen widget areas to activate/run. If none are selected in Display settings, act as if all are checked/enabled.
+	 *
 	 * @return array
 	 */
-	public function get_enabled_areas_simple() {
-		$all_available = $this->get_all_areas();
-		
+	protected function get_enabled_areas_simple() {
+		$all_available = $this->get_all_areas_simple();
+
 		$enabled_areas = (array) tribe_get_option( $this->option_key_enabled_areas, $all_available );
-		
-		foreach ( $enabled_areas as $key => $value ) {
-			if ( ! array_key_exists( $value, $all_available ) ) {
-				unset( $enabled_areas[$key] );
-			}
-		}
-		
+
 		return $enabled_areas;
 	}
-		
+
 	/**
-	 * The chosen widget areas to activate/run. Full array data from get_all_areas()
-	 * 
+	 * The chosen widget areas to activate/run. If none are selected in Display settings, act as if all are checked/enabled.
+	 *
 	 * @return array
 	 */
-	public function get_enabled_areas_full_details() {
-		$all_available = $this->get_all_areas();
-		
-		$enabled_areas = $this->get_enabled_areas_simple();
-		
+	protected function get_enabled_areas_full_details() {
+		$all_available_assoc = $this->get_all_areas_assoc();
+
+		$enabled_areas = (array) $this->get_enabled_areas_simple();
+
 		$return = array();
-		
-		// if none selected, return all available
-		if ( empty( $enabled_areas ) ) {
-			$return = $all_available;
-		} else {
-			foreach ( $enabled_areas as $key => $value ) {
-				if ( array_key_exists( $value, $all_available ) ) {
-					$return[$value] = $all_available[$value];
-				}
+
+		foreach ( $enabled_areas as $value ) {
+			if ( array_key_exists( $value, $all_available_assoc ) ) {
+				$return[$value] = $all_available_assoc[$value];
 			}
 		}
-		
+
 		return $return;
 	}
-	
+
 	/**
 	 * Add options to Tribe settings page
 	 *
@@ -166,7 +226,7 @@ class Tribe__Extension__Calendar_Widget_Areas extends Tribe__Extension {
 
 		// Setup fields on the settings page
 		$setting_helper = new Tribe__Extension__Settings_Helper();
-		$options = $this->get_available_area_options();
+		$options        = $this->get_available_area_options();
 
 		$setting_helper->add_field(
 			$this->option_key_enabled_areas,
@@ -174,7 +234,6 @@ class Tribe__Extension__Calendar_Widget_Areas extends Tribe__Extension {
 				'type'            => 'checkbox_list',
 				'label'           => esc_html__( 'Widget Areas', 'tribe-extension' ),
 				'tooltip'         => esc_html__( 'Select which widget areas you want available on your site. Note: Unchecking all the boxes will not save. If you want all areas unchecked, just deactivate this extension.', 'tribe-extension' ),
-				'default'         => array_keys( $options ),
 				'validation_type' => 'options_multi',
 				'options'         => $options,
 			),
@@ -183,87 +242,118 @@ class Tribe__Extension__Calendar_Widget_Areas extends Tribe__Extension {
 			true
 		);
 	}
-	
+
 	/**
 	 * Extension initialization and hooks.
 	 */
 	public function init() {
 		// Register all widget areas
 		add_action( 'widgets_init', array( $this, 'register_sidebars' ) );
-		
+
 		add_action( 'admin_init', array( $this, 'add_settings' ) );
-		
-		$active_areas = $this->get_enabled_areas_full_details();
-		
-		foreach ( $active_areas as $key => $value ) {
-			$method = '';
-			$method = $value['method'];
-			if ( method_exists( $this, $method ) ) {
-				// add_filter or add_action
-				if ( ! empty( $value['filter'] ) && true === $value['filter'] ) {
-					add_filter( $key, array( $this, $method ) );
+
+		foreach ( $this->get_enabled_areas_full_details() as $value ) {
+			if ( method_exists( $this, $value['method'] ) ) {
+				if ( empty( $value['priority'] ) ) {
+					$priority = 10;
 				} else {
-					add_action( $key, array( $this, $method ) );
+					$priority = $value['priority'];
+				}
+
+				$priority = (int) $priority;
+
+				if ( ! empty( $value['filter'] ) ) {
+					add_filter( $value['hook'], array( $this, $value['method'] ), $priority );
+				} else {
+					add_action( $value['hook'], array( $this, $value['method'] ), $priority );
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * Register all widget areas.
 	 */
 	public function register_sidebars() {
-		$active_areas = $this->get_enabled_areas_full_details();
-		
-		foreach ( $active_areas as $key => $value ) {
-			register_sidebar (
-				array (
-					'name'			=> $value['name'],
-					'id'			=> "tec_ext_widget_areas__{$value['method']}",
-					'description'	=> $value['desc'],
+		foreach ( $this->get_enabled_areas_full_details() as $value ) {
+			register_sidebar(
+				array(
+					'name'        => $value['name'],
+					'id'          => "tec_ext_widget_areas__{$value['method']}",
+					'description' => $value['desc'],
 				)
 			);
-		}	
+		}
 	}
-	
-	
+
+
 	/**
-	 * "Before Calendar" Widget Area
+	 * Before Calendar widget area
 	 */
 	public function before_template() {
 		dynamic_sidebar( 'tec_ext_widget_areas__before_template' );
 	}
-	
-	
+
+
 	/**
-	 * "After Calendar" Widget Area
+	 * After Calendar widget area
 	 */
 	public function after_template() {
 		dynamic_sidebar( 'tec_ext_widget_areas__after_template' );
 	}
-	
-	
+
+
 	/**
-	 * "Before Event Single" Widget Area
+	 * Before Event Single widget area
 	 */
 	public function single_before_view() {
-		if ( ! is_singular( Tribe__Events__Main::POSTTYPE ) ) {
-			return false;
+		if ( is_singular( Tribe__Events__Main::POSTTYPE ) ) {
+			dynamic_sidebar( 'tec_ext_widget_areas__single_before_view' );
 		}
-		
-		dynamic_sidebar( 'tec_ext_widget_areas__single_before_view' );
 	}
-	
-	
+
 	/**
-	 * "After Event Single" Widget Area
+	 * Above Event Single Description widget area
+	 */
+	public function single_event_before_the_content() {
+		dynamic_sidebar( 'tec_ext_widget_areas__single_event_before_the_content' );
+	}
+
+	/**
+	 * Below Event Single Description widget area
+	 */
+	public function single_event_after_the_content() {
+		dynamic_sidebar( 'tec_ext_widget_areas__single_event_after_the_content' );
+	}
+
+	/**
+	 * Above Event Single Details widget area
+	 */
+	public function single_event_before_the_meta() {
+		dynamic_sidebar( 'tec_ext_widget_areas__single_event_before_the_meta' );
+	}
+
+	/**
+	 * Below Event Single Details EARLY widget area
+	 */
+	public function single_event_after_the_meta_early() {
+		dynamic_sidebar( 'tec_ext_widget_areas__single_event_after_the_meta_early' );
+	}
+
+	/**
+	 * Below Event Single Details LATE widget area
+	 */
+	public function single_event_after_the_meta_late() {
+		dynamic_sidebar( 'tec_ext_widget_areas__single_event_after_the_meta_late' );
+	}
+
+	/**
+	 * After Event Single widget area
 	 */
 	public function single_after_view() {
 		if ( ! is_singular( Tribe__Events__Main::POSTTYPE ) ) {
-			return false;
+			dynamic_sidebar( 'tec_ext_widget_areas__single_after_view' );
 		}
-		
-		dynamic_sidebar( 'tec_ext_widget_areas__single_after_view' );
 	}
-	
+
 }
