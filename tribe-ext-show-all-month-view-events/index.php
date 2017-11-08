03fd1265fbe300344e74fcee8b380e288c12b7b6
Clifford Paulick <tko@tourkick.com>
2017-11-08 16:39:03 -0600

    version 1.0.1

--- tribe-ext-show-all-month-view-events/index.php
+++ tribe-ext-show-all-month-view-events/index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     The Events Calendar Extension: Show All Month View Events
  * Description:     An extension that, when activated, will not limit the number of events shown in each day in the Month View.
- * Version:         1.0.0
+ * Version:         1.0.1
  * Extension Class: Tribe__Extension__Show_All_Month_View_Events
  * Author:          Modern Tribe, Inc.
  * Author URI:      http://m.tri.be/1971
@@ -29,13 +29,14 @@ class Tribe__Extension__Show_All_Month_View_Events extends Tribe__Extension {
 	public function construct() {
 		$this->add_required_plugin( 'Tribe__Events__Main', '4.3.3' );
 		$this->set_url( 'https://theeventscalendar.com/extensions/display-all-events-on-a-day-in-month-view/' );
+		$this->set_version( '1.0.1' );
 	}
 
 	/**
 	 * Extension initialization and hooks.
 	 */
 	public function init() {
-		add_filter( 'tribe_events_month_day_limit', 'remove_limit' );
+		add_filter( 'tribe_events_month_day_limit', array( $this, 'remove_limit' ) );
 	}
 
 	/**
